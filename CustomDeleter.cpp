13492d6dd00dc6ea54c531a7d90ab3c6be752129
wjdgh2910a <wjdgh2910a@naver.com>
2024-11-09 23:38:21 +0900

    a

--- CustomDeleter.cpp
+++ CustomDeleter.cpp
@@ -1,14 +1,57 @@
 #include<iostream>
-#include<vector>
+#include<queue>
+#include<conio.h>
+
+
+struct Object
+{
+	static int s_iData;
+	int m_iData;
+	Object() : m_iData(++s_iData) {}
+	~Object() { std::cout << "Release Object(" << m_iData << ")" << std::endl; }
+	void Print() { std::cout << "Object(" << m_iData << ")" << std::endl; }
+};
+int Object::s_iData = 0;
+std::queue<std::shared_ptr<Object>> ObjectQueue;
+bool DeleteFlag = false;
+
+void Push(Object* object)
+{
+	if (DeleteFlag)
+		delete object;
+	else
+	{
+		ObjectQueue.push(std::shared_ptr<Object>(object, Push));
+		std::cout << "Pool In Object(" << object->m_iData << ")" << std::endl;
+	}
+}
 
 void main()
 {
-	std::vector<std::unique_ptr<int>> UniqueVector;
-	UniqueVector.push_back(std::make_unique<int>(1));
-	UniqueVector.push_back(std::make_unique<int>(2));
-	UniqueVector.push_back(std::make_unique<int>(3));
-	std::for_each(UniqueVector.begin(), UniqueVector.end(), [](std::unique_ptr<int>* ptr) 
+	for (int i = 0; i < 5; i++)
+	{
+		ObjectQueue.push(std::shared_ptr<Object>(new Object, Push));
+	}
+
+	int oldclock = clock();
+	while (true)
+	{
+		if (_kbhit())
 		{
-			std::cout << *ptr << std::endl;
-		})
+			char ch = _getch();
+			if (ch == 27)
+			{
+				DeleteFlag = true;
+				break;
+			}
+		}
+		if (clock() - oldclock >= 1000)
+		{
+			std::shared_ptr<Object> object = ObjectQueue.front();
+			ObjectQueue.pop();
+			object->Print();
+			object = nullptr;
+			oldclock = clock();
+		}
+	}
 }
